e2488c40f61e756e33aeffacc86f7d146326374d
Saikanhaiya21 <saikanhaiya36@gmail.com>
2025-09-14 15:58:55 +0530

    Setup express

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+
+const app = express();
+const port = 8080;
+
+app.get('/test', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+
